70b1440d61be9d21f2cf09a17b64bfe6e1e9fd4f
Evan <evanmckinney03@gmail.com>
2024-09-21 03:16:34 -0400

    Clicking on SVG creates a circle

--- graphs.js
+++ graphs.js
@@ -1,10 +1,23 @@
 
+const CIRCLE_R = 8;
 
 window.onload = init;
 
 function init() {
   let svg = document.getElementById('svg');
+  let id = 0;
   svg.addEventListener('mousedown', function(e) {
-    console.log([e.clientX, e.clientY])
+    createNode(e.clientX, e.clientY, id++);
   });
+}
+
+function createNode(x, y, id) {  
+  let svg = document.getElementById('svg');
+  let circleSVG = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+  circleSVG.setAttribute('r', CIRCLE_R);
+  circleSVG.setAttribute('cx', x);
+  circleSVG.setAttribute('cy', y);
+  circleSVG.setAttribute('class', 'node');
+  circleSVG.setAttribute('id', 'node' + id);
+  svg.appendChild(circleSVG);
 }
