059f139b46d0ea978b1db218c10dc0427b2f1df1
Evan <evanmckinney03@gmail.com>
2024-09-21 04:08:26 -0400

    Double clicking now creates a node, clicking a node selects it

--- graphs.js
+++ graphs.js
@@ -6,8 +6,15 @@ window.onload = init;
 function init() {
   let svg = document.getElementById('svg');
   let id = 0;
-  svg.addEventListener('mousedown', function(e) {
+  //create a rectangle to fill the SVG background
+  let background = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+  background.setAttribute('width', svg.getAttribute('width'));
+  background.setAttribute('height', svg.getAttribute('height'));
+  background.setAttribute('id', 'background');
+  svg.appendChild(background);
+  background.addEventListener('dblclick', function(e) {
     createNode(e.clientX, e.clientY, id++);
+    console.log('background dblclicked');
   });
 }
 
@@ -17,7 +24,13 @@ function createNode(x, y, id) {
   circleSVG.setAttribute('r', CIRCLE_R);
   circleSVG.setAttribute('cx', x);
   circleSVG.setAttribute('cy', y);
-  circleSVG.setAttribute('class', 'node');
+  circleSVG.classList.add('node');
+  circleSVG.classList.add('nodeUnselected');
   circleSVG.setAttribute('id', 'node' + id);
+  circleSVG.addEventListener('mousedown', function() {
+    console.log(this.getAttribute('id'));
+    this.classList.add('nodeSelected');
+    this.classList.remove('nodeUnselected');
+  });
   svg.appendChild(circleSVG);
 }
